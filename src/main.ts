b0c0cdff9a56886bdd246e27caeaa454f7b8a0b5
Dani <daniyil.kud@gmail.com>
2023-10-01 22:09:07 +0200

    finish

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 // import { AnalyticsService } from './app/shared/analytics.service';
 import { environment } from './environments/environment';
+import { AppRoutingModule } from './app/app-routing.module';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    // AnalyticsService,
-  ],
+    providers: [
+        // AnalyticsService,
+        importProvidersFrom(AppRoutingModule),
+    ],
 });
